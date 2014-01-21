9bda664e9b539b100ab1e39e88e4efcdec49bde7
FiliH <v.anashkevich@gmail.com>
2014-01-21 17:57:39 +0300

    111

--- Config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('TWITTER_KEY', 'J20dV1N2THWbIkOKjU9Kw');
-define('TWITTER_SECRET', 'BRy2gtjUJJpIbbVsko42DnA5ZfdyrhCFHwvEanzhibU');
-define("OAUTH_TOKEN", "1377597367-66BkZ5SIzt0WPnzdIjSor3WbHKlLJOQtke3C7R3");
-define("OAUTH_SECRET", "ePgtd0JKN7OdC119aFe8ZWLZi53zE7byicn16Gf54Tsyy");
-?>
