a8bc8388dad1c17e73ca360722f3959163f13df5
FiliH <v.anashkevich@gmail.com>
2014-01-24 13:32:31 +0300

    new files

---
+++ processing/print_p.php
@@ -0,0 +1,22 @@
+<?php
+function p($value, $emp, $print){
+    if(!empty($emp)){
+        print ($emp."  :");
+    }
+
+    if(!empty($value)){
+        print("<pre>");
+        switch($print){
+            case 'v': var_dump($value);
+                break;
+
+            case 'p': print($value);
+                break;
+
+            case 'pr': print_r($value);
+                break;
+        }
+        print("</pre>");
+    }
+}
+?>
